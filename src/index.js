abd98d768180c259bb496a288f5e42ce8d714233
YeshPatel23 <dipci00004019@student.maseno.ac.ke>
2022-04-13 11:19:23 +0300

    Updated Routers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 require("./db/mongoose");
-const Task = require("./models/task");
-const User = require("./models/user");
-
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 // app.post("/tasks", (req, res) => {
 //   const task = new Task(req.body);
@@ -20,40 +21,6 @@ app.use(express.json());
 //       res.status(400).send(error);
 //     });
 // });
-
-app.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
-
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.send(tasks);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const task = await Task.findById(_id);
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
 
 // app.get("/tasks", (req, res) => {
 //   Task.find({})
@@ -94,40 +61,6 @@ app.get("/tasks/:id", async (req, res) => {
 //     });
 // });
 
-app.post("/users", async (req, res) => {
-  const user = new User(req.body);
-
-  try {
-    await user.save();
-    res.status(200).send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
 // app.get("/users", (req, res) => {
 //   User.find({})
 //     .then((users) => {
@@ -153,64 +86,6 @@ app.get("/users/:id", async (req, res) => {
 //       res.status(500).send();
 //     });
 // });
-
-app.patch("/users/:id", async (req, res) => {
-  const updates = Object.keys(req.body); //converts json to javascript
-  const allowedUpdates = ["name", "email", "password", "age"]; // specifing what propertises can be editable
-  const isValidOperation = updates.every(
-    (
-      update ///checking if each propertises is similar to the one we want to update (also is a boolean type unlike map that can be manupilated)
-    ) => allowedUpdates.includes(update)
-  );
-
-  if (!isValidOperation) {
-    return res.status(400).send({ error: "Invalid updates!" });
-  }
-
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-app.patch("/tasks/:id", async (req, res) => {
-  const updates = Object.keys(req.body); //converts json to javascript
-  const allowedUpdates = ["title", "amount", "date"]; // specifing what propertises can be editable
-  const isValidOperation = updates.every(
-    (
-      update ///checking if each propertises is similar to the one we want to update (also is a boolean type unlike map that can be manupilated)
-    ) => allowedUpdates.includes(update)
-  );
-
-  if (!isValidOperation) {
-    return res.status(400).send({ error: "Invalid updates!" });
-  }
-
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!task) {
-      return res.status(404).send();
-    }
-
-    res.send(task);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
 
 app.listen(port, () => {
   console.log("server is running on port " + port);
---
+++ src/routers/user.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const User = require("../models/user");
+const router = new express.Router();
+
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body); //converts json to javascript
+  const allowedUpdates = ["name", "email", "password", "age"]; // specifing what propertises can be editable
+  const isValidOperation = updates.every(
+    (
+      update ///checking if each propertises is similar to the one we want to update (also is a boolean type unlike map that can be manupilated)
+    ) => allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(505).send();
+  }
+});
+
+module.exports = router;
