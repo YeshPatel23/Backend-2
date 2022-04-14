c84bff02e9e30217cf9e11929bcf2424099c3bbf
YeshPatel23 <dipci00004019@student.maseno.ac.ke>
2022-04-14 10:27:55 +0300

    updated user file

--- src/models/user.js
+++ src/models/user.js
@@ -41,6 +41,22 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("unable to login");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Wrong Password");
+  }
+
+  return user;
+};
+
 userSchema.pre("save", async function (next) {
   const user = this;
 
